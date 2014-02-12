5eb1aa243097e4a3fe529c921e665567d62cb980
chrishey <chrishey87@hotmail.com>
2014-02-12 13:07:08 +0000

    Added timer to tick and have a callback to write out the time every second

--- SignalR.NotAChatDemo/SignalR.NotAChatDemo/Time.cs
+++ SignalR.NotAChatDemo/SignalR.NotAChatDemo/Time.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading;
 
 namespace SignalR.NotAChatDemo
 {
 	public class Time : Hub
 	{
+        private Timer _timer;
+
 		public void TellMeTheTime()
 		{
-			Clients.All.printTime(DateTime.Now.ToString());
+            if (_timer != null)
+            {
+                return;
+            }
+
+            _timer = new Timer(GetTime, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
 		}
+
+        private void GetTime(object sender)
+        {
+            Clients.All.printTime(DateTimeOffset.UtcNow);
+        }
 	}
 }
