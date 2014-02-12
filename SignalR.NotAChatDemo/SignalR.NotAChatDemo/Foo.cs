b0bc387ce4cfb108ee6f0c5e36651f059e0c79e2
chrishey <chrishey87@hotmail.com>
2014-02-12 13:45:15 +0000

    Renamed index to print date, added new hub and page for sending data to the server and back

---
+++ SignalR.NotAChatDemo/SignalR.NotAChatDemo/Foo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading;
+
+namespace SignalR.NotAChatDemo
+{
+	public class Foo : Hub
+	{
+        private const string ERROR_MESSAGE = "Sorry - your name is not recognised!";
+        private const string DEFAULT_MESSAGE = "Hello {0} {1}!";
+
+		public void EnterName(dynamic data)
+		{
+            string message = ERROR_MESSAGE;
+
+            if (data != null)
+            {
+                message = string.Format(DEFAULT_MESSAGE, data.firstName, data.lastName);
+            }
+
+            PrintName(message);
+		}
+
+        private void PrintName(string message)
+        {
+            Clients.All.printName(message);
+        }
+	}
+}
