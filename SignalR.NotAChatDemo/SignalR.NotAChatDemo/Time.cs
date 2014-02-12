7636844f21cb1ec175a26e8b02d5b48c061d141a
Chris Hey <chrishey87@hotmail.com>
2014-02-12 11:38:00 +0000

    Added correct version of OWIN, added hub class and OWIN startup class, also added html page to call and display message

---
+++ SignalR.NotAChatDemo/SignalR.NotAChatDemo/Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.NotAChatDemo
+{
+	public class Time : Hub
+	{
+		public void TellMeTheTime()
+		{
+			Clients.All.printTime(DateTime.Now.ToString());
+		}
+	}
+}
